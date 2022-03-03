97245a1a1a380fdc2b8a7cd1d4fe4e771bc07cbc
Randriatavy Mandresy <mandresyrvy@gmail.com>
2022-03-03 16:26:20 +0100

    Commentaries added

--- src/Systeme/Settings.php
+++ src/Systeme/Settings.php
@@ -4,19 +4,22 @@ namespace Systeme;
 
 class Settings
 {
+    // Database
     protected const DB_NAME = 'beastmod_db';
     protected const DB_HOST = 'localhost';
     protected const DB_USER = 'root';
     protected const DB_PASS = '';
 
+    // Website
     protected const WEBSITE = 'RetroClub';
     protected const NICKNAME = 'les sans frontières';
     protected const FAN = 'de tous les retros';
-
+        // About Website
     protected const CMS_VERSION = 'R2.0 (Alpha)';
     protected const CMS_AUTHOR = 'Kamobbah';
 
     // URLS
+    protected const URL_USER_PROFIL = '/users/p/';
 
     // URLS - IMAGES
 }
